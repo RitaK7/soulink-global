725484c256f6396ff9cad902c79364996f44be06
RitaK7 <126379283+RitaK7@users.noreply.github.com>
2026-04-23 18:08:10 +0300

    kiop

--- firebase-config.js
+++ firebase-config.js
@@ -12,7 +12,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyCb0TOXlvrP1dQe16T6RNRvAg8lwu9Pnf4",
   authDomain: "soulink-342bb.firebaseapp.com",
   projectId: "soulink-342bb",
-  storageBucket: "soulink-342bb.firebasestorage.app",
+  storageBucket: "soulink-342bb.appspot.com",
   messagingSenderId: "541312933178",
   appId: "1:541312933178:web:239cbbb60e68b183f48403"
 };
