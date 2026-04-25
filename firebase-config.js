5dc4901f8dd589149ac3da37d86454183f20fd60
RitaK7 <126379283+RitaK7@users.noreply.github.com>
2026-04-25 16:44:20 +0300

    iiiii

--- firebase-config.js
+++ firebase-config.js
@@ -13,10 +13,10 @@ import {
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-storage.js";
 
 const firebaseConfig = {
-  apiKey: "TAVO_API_KEY",
+  apiKey: "AIzaSyC5NOF2WAQM7-9SoEtm_9Mc3QuX0h2m1lg",
   authDomain: "soulink-342bb.firebaseapp.com",
   projectId: "soulink-342bb",
-  storageBucket: "soulink-342bb.appspot.com",
+  storageBucket: "soulink-342bb.firebasestorage.app",
   messagingSenderId: "541312933178",
   appId: "1:541312933178:web:239cbbb60e68b183f48403"
 };
